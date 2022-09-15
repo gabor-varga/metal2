bda2108e952d153fd865dee7f0dcfaa1f3af52f6
Gabor Varga <gabor.varga@ext.esa.int>
2022-09-15 17:25:30 +0100

    expression template mock, more tests

--- metal/Dual.hpp
+++ metal/Dual.hpp
@@ -9,10 +9,49 @@ template< typename Value, typename Deriv >
 class Dual
 {
 public:
+    // template< typename T, typename V >
+    // Dual( T&& value, V&& deriv ) requires
+    //     std::is_same_v< std::remove_cvref_t< T >, Value > && std::is_same_v< std::remove_cvref_t< V >, Deriv >
+    //     : value_{ std::move( value ) }, deriv_{ std::move( deriv ) }
+    // {
+    // }
+
+    Dual( const Value& value, const Deriv& deriv )
+        : value_{ value }
+        , deriv_{ deriv }
+    {
+    }
+
+    Dual( const Value& value, Deriv&& deriv )
+        : value_{ value }
+        , deriv_{ std::move( deriv ) }
+    {
+    }
+
+    Dual( Value&& value, const Deriv& deriv )
+        : value_{ std::move( value ) }
+        , deriv_{ deriv }
+    {
+    }
+
+    Dual( Value&& value, Deriv&& deriv )
+        : value_{ std::move( value ) }
+        , deriv_{ std::move( deriv ) }
+    {
+    }
+
     template< typename T, typename V >
-    Dual( T&& value, V&& deriv ) requires
-        std::is_same_v< std::remove_cvref_t< T >, Value > && std::is_same_v< std::remove_cvref_t< V >, Deriv >
-        : value_{ std::move( value ) }, deriv_{ std::move( deriv ) }
+    Dual( T value, V deriv ) requires
+        (!std::is_same_v< std::remove_cvref_t< T >, Value >) && (!std::is_same_v< std::remove_cvref_t< V >, Deriv >)
+        : value_{ value }
+        , deriv_{ deriv }
+    {
+    }
+
+    template< typename T, typename V >
+    Dual( const Dual< T, V >& other )
+        : value_{ other.value() }
+        , deriv_{ other.deriv() }
     {
     }
 
@@ -22,9 +61,34 @@ public:
     // {
     // }
 
+    const Value& value() const { return value_; }
+    const Deriv& deriv() const { return deriv_; }
+
+    template< typename V1, typename D1, typename V2, typename D2 >
+    friend auto operator+( const Dual< V1, D1 >& x, const Dual< V2, D2 >& y );
+
+    // template< typename V1, typename D1, typename V2, typename D2 >
+    // friend auto operator+( Dual< V1, D1 >&& x, Dual< V2, D2 >&& y );
+
 private:
     Value value_;
     Deriv deriv_;
 };
+
+template< typename V1, typename D1, typename V2, typename D2 >
+auto operator+( const Dual< V1, D1 >& x, const Dual< V2, D2 >& y )
+{
+    using V3 = decltype( std::declval< V1 >() + std::declval< V2 >() );
+    using D3 = decltype( std::declval< D1 >() + std::declval< D2 >() );
+    return Dual< V3, D3 >( x.value_ + y.value_, x.deriv_ + y.deriv_ );
+}
+
+// template< typename V1, typename D1, typename V2, typename D2 >
+// auto operator+( Dual< V1, D1 >&& x, Dual< V2, D2 >&& y )
+// {
+//     using V3 = decltype( std::declval< V1 >() + std::declval< V2 >() );
+//     using D3 = decltype( std::declval< D1 >() + std::declval< D2 >() );
+//     return Dual< V3, D3 >( x.value_ + y.value_, x.deriv_ + y.deriv_ );
+// }
 
 }
--- tests/DualTest.cpp
+++ tests/DualTest.cpp
@@ -31,22 +31,44 @@ std::ostream& operator<<( std::ostream& os, const FunctionCalls& object )
 }
 
 
-template< int Id >
-struct Mock
+template< typename Derived >
+struct MockBase
 {
-    Mock( int value )
+    int value() const { return static_cast< const Derived& >( *this ).value(); }
+};
+
+template< typename Derived >
+std::ostream& operator<<( std::ostream& os, const MockBase< Derived >& object )
+{
+    os << object.value();
+    return os;
+}
+
+
+template< int Id >
+struct Mock : public MockBase< Mock< Id > >
+{
+    explicit Mock( int value )
         : value_{ std::make_unique< int >( value ) }
     {
         function_calls.ctor++;
     }
 
+    template< typename Derived >
+    explicit Mock( const MockBase< Derived >& other )
+        : value_{ std::make_unique< int >( other.value() ) }
+    {
+    }
+
     ~Mock() { function_calls.dtor++; }
 
     Mock( const Mock& other )
-        : value_{ std::make_unique< int >( *other.value_.get() ) }
+        : value_{ std::make_unique< int >( other.value() ) }
     {
         function_calls.copy++;
     }
+
+    // Mock( const Mock& other ) = delete;
 
     Mock( Mock&& other ) noexcept
         : value_{ std::move( other.value_ ) }
@@ -56,7 +78,7 @@ struct Mock
 
     Mock& operator=( const Mock& other )
     {
-        value_ = other.value_;
+        value_ = std::make_unique< int >( other.value() );
         function_calls.cass++;
     }
 
@@ -66,6 +88,16 @@ struct Mock
         function_calls.mass++;
     }
 
+    bool is_valid() const { return value_ != nullptr; }
+
+    int value() const
+    {
+        if ( value_ )
+            return *value_;
+        else
+            throw std::runtime_error( "Invalid Mock object" );
+    }
+
     static void reset() { function_calls = FunctionCalls{}; }
 
     std::unique_ptr< int > value_ = nullptr;
@@ -73,8 +105,38 @@ struct Mock
     static FunctionCalls function_calls;
 };
 
+// template< int Id >
+// std::ostream& operator<<( std::ostream& os, const Mock< Id >& object )
+// {
+//     os << object.value();
+//     return os;
+// }
+
 using Value = Mock< 0 >;
 using Deriv = Mock< 1 >;
+
+template< int Id >
+class MockAddition : public MockBase< MockAddition< Id > >
+{
+public:
+    MockAddition( const MockBase< Mock< Id > >& lhs, const MockBase< Mock< Id > >& rhs )
+        : lhs_{ lhs }
+        , rhs_{ rhs }
+    {
+    }
+
+    int value() const { return lhs_.value() + rhs_.value(); }
+
+private:
+    const MockBase< Mock< Id > >& lhs_;
+    const MockBase< Mock< Id > >& rhs_;
+};
+
+template< int Id >
+auto operator+( const MockBase< Mock< Id > >& x, const MockBase< Mock< Id > >& y )
+{
+    return MockAddition< Id >{ x, y };
+}
 
 
 template<>
@@ -86,33 +148,37 @@ FunctionCalls Deriv::function_calls = FunctionCalls{};
 
 TEST_CASE( "Test number of function calls with different constructor arguments" )
 {
-    const auto test_function_calls = []( const FunctionCalls& expected_function_calls )
+    const auto test_function_calls
+        = []( const FunctionCalls& expected_value_function_calls, const FunctionCalls& expected_deriv_function_calls )
     {
-        REQUIRE( Value::function_calls == expected_function_calls );
-        REQUIRE( Deriv::function_calls == expected_function_calls );
+        REQUIRE( Value::function_calls == expected_value_function_calls );
+        REQUIRE( Deriv::function_calls == expected_deriv_function_calls );
         Value::reset();
         Deriv::reset();
     };
 
     const auto perfect_move = FunctionCalls{ 1, 2, 0, 1, 0, 0 };
-    const auto non_perfect_copy = FunctionCalls{ 1, 2, 1, 0, 0, 0 };
-
     SECTION( "Constructor with both rvalue args" )
     {
         {
             metal::Dual< Value, Deriv > dual{ Value{ 1 }, Deriv{ 2 } };
         }
-        test_function_calls( perfect_move );
+        test_function_calls( perfect_move, perfect_move );
     }
 
+    const auto non_perfect_copy = FunctionCalls{ 1, 2, 1, 0, 0, 0 };
     SECTION( "Constructor with both lvalue args" )
     {
         {
             Value value{ 1 };
             Deriv deriv{ 1 };
             metal::Dual< Value, Deriv > dual{ value, deriv };
+
+            // Make sure value and deriv objects are still valid
+            REQUIRE( value.is_valid() );
+            REQUIRE( deriv.is_valid() );
         }
-        test_function_calls( perfect_move );
+        test_function_calls( non_perfect_copy, non_perfect_copy );
     }
 
     SECTION( "Constructor with lvalue and rvalue args" )
@@ -120,8 +186,11 @@ TEST_CASE( "Test number of function calls with different constructor arguments" 
         {
             Value value{ 1 };
             metal::Dual< Value, Deriv > dual{ value, Deriv{ 2 } };
+
+            // Make sure value is still valid
+            REQUIRE( value.is_valid() );
         }
-        test_function_calls( perfect_move );
+        test_function_calls( non_perfect_copy, perfect_move );
     }
 
     SECTION( "Constructor with rvalue and lvalue args" )
@@ -129,17 +198,65 @@ TEST_CASE( "Test number of function calls with different constructor arguments" 
         {
             Deriv deriv{ 1 };
             metal::Dual< Value, Deriv > dual{ Value{ 1 }, deriv };
+
+            // Make sure deriv is still valid
+            REQUIRE( deriv.is_valid() );
         }
-        test_function_calls( perfect_move );
+        test_function_calls( perfect_move, non_perfect_copy );
     }
 
-    SECTION( "Constructor with both lvalue args" )
+    SECTION( "Constructor with both const lvalue args" )
     {
         {
             const Value value{ 1 };
             const Deriv deriv{ 1 };
             metal::Dual< Value, Deriv > dual{ value, deriv };
         }
-        test_function_calls( non_perfect_copy );
+        test_function_calls( non_perfect_copy, non_perfect_copy );
+    }
+
+    const auto perfect_move_ctor = FunctionCalls{ 1, 3, 0, 2, 0, 0 };
+    SECTION( "Move constructor test" )
+    {
+        {
+            metal::Dual< Value, Deriv > dual{ Value{ 1 }, Deriv{ 2 } };
+            metal::Dual< Value, Deriv > dual2{ std::move( dual ) };
+        }
+        test_function_calls( perfect_move_ctor, perfect_move_ctor );
+    }
+
+    const auto non_perfect_copy_ctor = FunctionCalls{ 1, 3, 1, 1, 0, 0 };
+    SECTION( "Copy constructor test" )
+    {
+        {
+            const metal::Dual< Value, Deriv > dual{ Value{ 1 }, Deriv{ 2 } };
+            metal::Dual< Value, Deriv > dual2{ dual };
+        }
+        test_function_calls( non_perfect_copy_ctor, non_perfect_copy_ctor );
+    }
+
+    const auto perfect_binary_op_move = FunctionCalls{ 2, 4, 0, 2, 0, 0 };
+    SECTION( "Test binary operators" )
+    {
+        {
+            metal::Dual< Value, Deriv > x{ Value{ 1 }, Deriv{ 2 } };
+            metal::Dual< Value, Deriv > y{ Value{ 3 }, Deriv{ 4 } };
+            auto z = x + y;
+
+            REQUIRE( x.value().is_valid() );
+            REQUIRE( x.deriv().is_valid() );
+            REQUIRE( y.value().is_valid() );
+            REQUIRE( y.deriv().is_valid() );
+
+            // metal::Dual< Value, Deriv > z = metal::Dual< Value, Deriv >{ Value{ 1 }, Deriv{ 2 } }
+            //     + metal::Dual< Value, Deriv >{ Value{ 3 }, Deriv{ 4 } };
+
+            // auto z = metal::Dual< Value, Deriv >{ Value{ 1 }, Deriv{ 2 } }
+            //     + metal::Dual< Value, Deriv >{ Value{ 3 }, Deriv{ 4 } };
+
+            REQUIRE( z.value().value() == 4 );
+            REQUIRE( z.deriv().value() == 6 );
+        }
+        test_function_calls( perfect_binary_op_move, perfect_binary_op_move );
     }
 }
